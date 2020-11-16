590fd05529071ef3144eac6f9037246b1074e0a7
callmewhy <whywanghai@gmail.com>
2020-11-16 17:42:52 +0800

    no message

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -3,8 +3,8 @@ import { ChainId, Token } from '@uniswap/sdk'
 export const MeterTokens: { [chainId in ChainId]: { [address: string]: Token } } = {
   [ChainId.MAINNET]: {},
   [ChainId.RINKEBY]: {
-    '0x4f6D94accF73713968f6D1B3d191A05762BfD2c1': new Token(
-      ChainId.MAINNET,
+    '0xBeE85b7b676f9306803B6DFC09F024c30a7A2a1e': new Token(
+      ChainId.RINKEBY,
       '0xBeE85b7b676f9306803B6DFC09F024c30a7A2a1e',
       18,
       'EMTR',
