0df43ae8ccb1c3f6c8c509a91411745f12cdf63f
callmewhy <whywanghai@gmail.com>
2020-12-07 02:32:49 +0800

    no message

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -174,15 +174,6 @@ export default function Pool() {
               </EmptyProposals>
             ) : allV2PairsWithLiquidity?.length > 0 ? (
               <>
-                <ButtonSecondary>
-                  <RowBetween>
-                    <ExternalLink href={'https://uniswap.info/account/' + account}>
-                      Account analytics and accrued fees
-                    </ExternalLink>
-                    <span> ↗</span>
-                  </RowBetween>
-                </ButtonSecondary>
-
                 {allV2PairsWithLiquidity.map(v2Pair => (
                   <FullPositionCard key={v2Pair.liquidityToken.address} pair={v2Pair} />
                 ))}
