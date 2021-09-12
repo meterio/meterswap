0c55155c240d728613a99ef7ae9f84945349392c
Simon Zhang <zhanghan.simon@gmail.com>
2021-09-12 22:59:43 +0800

    use new url for meter/geyser-v2 subgraph

--- src/queries/client.ts
+++ src/queries/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const uri = 'https://farm-graph.voltswap.finance/subgraphs/name/meter/geyser-V2';
+const uri = 'https://graph.voltswap.finance/subgraphs/name/meter/geyser-v2';
 
 export const client = new ApolloClient({
   uri,
