f140bbc06606c30640f0a056de05a71fb6af43bf
philipappiah <pappiahkubi1@gmail.com>
2022-06-06 22:15:54 +0100

    change theta subgraph url

--- src/queries/thetaclient.ts
+++ src/queries/thetaclient.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const uri = 'https://geyser-graph-on-theta.voltswap.finance/subgraphs/name/theta/geyser-v2';
+const uri = 'https://graph-theta.voltswap.finance/subgraphs/name/theta/geyser-v2';
 
 export const tclient = new ApolloClient({
   uri,
@@ -9,7 +9,7 @@ export const tclient = new ApolloClient({
 
 
 
-const UNI_GRAPH_URI = 'https://geyser-graph-on-theta.voltswap.finance/subgraphs/name/theta/uniswap-v2-subgraph';
+const UNI_GRAPH_URI = 'https://graph-theta.voltswap.finance/subgraphs/name/theta/uniswap-v2-subgraph';
 export const uniClient = new ApolloClient({
   uri: UNI_GRAPH_URI,
   cache: new InMemoryCache()
