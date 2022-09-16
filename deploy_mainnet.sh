f885dd4f8a830e2b18e5eb814ecfbbe4ee88acda
Simon.Z <zhanghan.simon@gmail.com>
2022-09-16 08:36:13 -0400

    move s3 from voltswap-finance to v1-voltswap

--- deploy_mainnet.sh
+++ deploy_mainnet.sh
@@ -4,8 +4,8 @@
 yarn build
 
 # copy dist to mainnet S3
-aws s3 sync ./build/ s3://voltswap.finance/ --delete --acl public-read
+aws s3 sync ./build/ s3://v1-voltswap/ --delete --acl public-read
 
 # invalidate mainnet cloudfront cache
-aws cloudfront create-invalidation --distribution-id E2WA1QZUJZWSWY --paths "/*"
+aws cloudfront create-invalidation --distribution-id E1W5WMJML1VEUH --paths "/*"
 
